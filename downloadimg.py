3605fda4874fe337481bb8d368d5acb4a1561939
Kenn <KenMac@192-168-1-89.tpgi.com.au>
2020-09-16 19:37:20 +0800

    added multiple chapter download

--- downloadimg.py
+++ downloadimg.py
@@ -5,14 +5,9 @@ from tqdm import tqdm
 from bs4 import BeautifulSoup
 
 class DownloadImages:
-    def __init__(self,title,chapter,numchapters):
+    def __init__(self,title,chapter):
         self.title = title
         self.chapter = chapter
-        
-        if numchapters == None:
-            numchapters = 1
-        else:
-            self.numchapters = numchapters
 
     def getpath(self):
         path ='downloads'+ '//' +self.geturl().split('/')[-3] + '//' +self.geturl().split('/')[-2] 
@@ -25,28 +20,49 @@ class DownloadImages:
         url = 'https://www.readmng.com/{}/{}/all-pages'.format(newtitle,self.chapter)
         return url
 
-    def downloadimages(self):
-        response = requests.get(self.geturl())
-        soup = BeautifulSoup(response.text,'html.parser')
-
-        imageslinks = soup.html.body.findAll('img',{'src': re.compile('chapter_files')})
-        for lines in imageslinks:
-            if len(imageslinks)!=len(os.listdir(self.getpath())):
-                self.download(lines.get('src'))
+    def downloadimages(self, numchapters=None):
+        if numchapters == None:
+            numchapters = 1
+        
+        url = self.geturl()
+        path = self.getpath()
+        chapter = float(url.split('/')[-2])
+        for x in range(numchapters):
+            newtitle = self.title.lower().replace(' ','-')[0:-1]
+            tempchapter = float(self.chapter) - x 
+            if tempchapter.is_integer()==True:
+                chapter=int(tempchapter)
             else:
-                pass
+                chapter=tempchapter
+            path ='downloads'+ '//' +self.geturl().split('/')[-3] + '//' +str(chapter)
+            if not os.path.isdir(path):
+                os.makedirs(path)
+            url = 'https://www.readmng.com/{}/{}/all-pages'.format(newtitle,str(chapter))
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text,'html.parser')
 
-    def download(self, url):
+            imageslinks = soup.html.body.findAll('img',{'src': re.compile('chapter_files')})
+            for lines in imageslinks:
+                if len(imageslinks)!=len(os.listdir(path)):
+                    self.download(lines.get('src'), path)
+                else:
+                    pass
+
+    def download(self, url, path=None):
         """
         Downloads a file given an URL and puts it in the folder `pathname`
         """
+        if path==None:
+            usepath=self.getpath()
+        else:
+            usepath=path
         # if path doesn't exist, make that path dir
         # download the body of response by chunk, not immediately
         response = requests.get(url, stream=True)
         # get the total file size
         file_size = int(response.headers.get("Content-Length", 0))
         # get the file name
-        filename = os.path.join(self.getpath(), url.split("/")[-1])
+        filename = os.path.join(usepath, url.split("/")[-1])
         if not os.path.exists(filename):
             # progress bar, changing the unit to bytes instead of iteration (default by tqdm)
             progress = tqdm(response.iter_content(1024), f"Downloading {filename}", total=file_size, unit="B", unit_scale=True, unit_divisor=1024)
--- trialreadingfiles.py
+++ trialreadingfiles.py
@@ -17,7 +17,7 @@ file.close()
 mangalist = {
     'manga':[],
     'latestchapter':[],
-    'numchapters':[]
+    'numchapters':[],
 }
 
 for lines in data.split('\n'):
@@ -26,12 +26,15 @@ for lines in data.split('\n'):
         mangalist['manga'].append(splits[0])
         manga1 = Manga(splits[0])
         if splits[1].strip()!='':
-            mangalist['latestchapter'].append(lines.split('-')[1].strip())
-        elif splits[2].strip()!='':
-            mangalist['numchapters'].append(lines.split('-')[2].strip())
+            if manga1.getlatestchapternum() == splits[1].strip():
+                mangalist['latestchapter'].append(splits[1].strip())
+            else:
+                mangalist['latestchapter'].append(manga1.getlatestchapternum())
+        if splits[2].strip()!='':
+            mangalist['numchapters'].append(int(splits[2].strip()))
         else:
             mangalist['latestchapter'].append(manga1.getlatestchapternum())
-            mangalist['numchapters'].append(1)
+            mangalist['numchapters'].append(None)
     except:
         pass
 
@@ -39,7 +42,7 @@ newwrite = ''
 counter = 0
 for lines in mangalist['manga']:
     if counter<=len(mangalist['manga'])-1:
-        newwrite+=lines+ "- " +str(mangalist['latestchapter'][counter]) +"\n"
+        newwrite+=lines+ "- " +str(mangalist['latestchapter'][counter]) + " - " + str(mangalist['numchapters'][counter]) + "\n"
         counter+=1
 
 file = open(path, 'w', encoding='utf-8')
@@ -47,6 +50,5 @@ file.write(newwrite[:-1])
 file.close()
 
 for x in range(len(mangalist['manga'])):
-    download1 = downloadimg.DownloadImages(mangalist['manga'][x],mangalist['latestchapter'][x],mangalist['numchapter'][x])
-    download1.downloadimages()
-    #print(mangalist['manga'][x] + mangalist['latestchapter'][x])
+    download1 = downloadimg.DownloadImages(mangalist['manga'][x],mangalist['latestchapter'][x])
+    download1.downloadimages(mangalist['numchapters'][x])
